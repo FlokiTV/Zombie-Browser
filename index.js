1305503e7a3706e03692afa8bef427e90fa4ba59
Floki <dawysonw@gmail.com>
2021-03-26 10:56:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -61,26 +61,15 @@ const loadModule = url =>{
   })
 }
 
-const __log = e =>{
-  let styles = [
-      'background: linear-gradient(#D33106, #571402)'
-      , 'border: 1px solid #3E0E02'
-      , 'color: white'
-      , 'display: block'
-      , 'text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)'
-      , 'box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
-      , 'line-height: 40px'
-      , 'text-align: center'
-      , 'font-weight: bold'
-  ].join(';');
-  console.log('%c '+e, styles);
-}
+const __log = e => console.log(e);
 
 /**
  *  Setup Browser
  */
  ;(async () => {
-  __log("[START]")
+  __log(`[START] Timer: ${TIMER}`)
+  __log(`[SCRIPT] ${SCRIPT}`)
+  __log(`[CONTEXT] Size: ${SIZE}`)
   setupBrowser()
     .then(contexts => loop(contexts) )
   // 
